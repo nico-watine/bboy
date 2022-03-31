91e751ee28851fd65d9e31e76a860379b843d4c5
Nico Watine <nico@studioncreations.com>
2022-03-31 18:06:52 -0500

    Placements

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,7 +9,9 @@ body-id: index
 body-class: index
 ---
 <section class="splash grid-x grid-padding-x" style="height: 95vh; min-height: 700px;overflow: hidden;">
-	<div class="cell text-center prose-wrap">
-		<h1 class="ff-whoa"><span>bboy</span> lilou</h1>
+	<div class="cell prose-wrap">
+		<div class="prose text-center">
+			<h1 class="ff-whoa"><span>bboy</span> lilou</h1>
+		</div>
 	</div>
 </section>
