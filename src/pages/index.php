5ebc52c555a3cadcec58c26071449dcdcb44de14
Nico Watine <nico@studioncreations.com>
2022-03-31 17:56:30 -0500

    Progress on splash

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,6 +8,8 @@ og-image:
 body-id: index
 body-class: index
 ---
-<section class="splash grid-x grid-padding-x align-middle" style="height: 95vh;">
-	<div class="cell text-center"><h1 class="ff-whoa" style="font-size: 110px;color: #FEB545;transform: rotate(-4deg);">bboy</h1> <h1 class="ff-whoa" style="color: #FFFFFF;font-size: 200px;transform: rotate(-21deg);">lilou</h1></div>
+<section class="splash grid-x grid-padding-x" style="height: 95vh; min-height: 700px;overflow: hidden;">
+	<div class="cell text-center prose-wrap">
+		<h1 class="ff-whoa"><span>bboy</span> lilou</h1>
+	</div>
 </section>
