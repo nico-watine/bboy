3cb26a4632a3c331b44fed0ab86847ee9d2803b5
Nico Watine <nico@studioncreations.com>
2022-04-01 14:40:52 -0500

    Move inlines to scss

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,7 +8,7 @@ og-image:
 body-id: index
 body-class: index
 ---
-<section class="splash grid-x grid-padding-x" style="height: 95vh; min-height: 700px;overflow: hidden;">
+<section class="splash grid-x grid-padding-x">
 	<div class="cell">
 		<div class="prose-wrap text-center">
 			<h1 class="ff-whoa"><span>bboy</span> lilou</h1>
