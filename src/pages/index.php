54428ee08eb116a7ee75dca79112d3989dfb3b5d
Nico Watine <nico@studioncreations.com>
2022-04-01 14:37:46 -0500

    Change background design to flag

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,8 +9,8 @@ body-id: index
 body-class: index
 ---
 <section class="splash grid-x grid-padding-x" style="height: 95vh; min-height: 700px;overflow: hidden;">
-	<div class="cell prose-wrap">
-		<div class="prose text-center">
+	<div class="cell">
+		<div class="prose-wrap text-center">
 			<h1 class="ff-whoa"><span>bboy</span> lilou</h1>
 		</div>
 	</div>
