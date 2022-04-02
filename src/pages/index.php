d7c658151f2f53870a8beb12d332dd08779e7827
Nico Watine <nico@studioncreations.com>
2022-04-01 20:58:48 -0500

    More

--- src/pages/index.php
+++ src/pages/index.php
@@ -23,7 +23,9 @@ body-class: index
 				<img style="border-radius: 20px;" src="/img/lilou-bio.jpg" alt="Lilou" title="Lilou">
 			</div>
 			<div class="cell large-7">
-
+				<h2 class="ff-whoa mb-40" style="font-size: 70px;color: #323232;">about lilou</h2>
+				<h3 class="font-weight-bold text-uppercase mb-40" style="font-size: 26px;">Ali Ramdani, better known by his stage name Lilou, is a French b-boy breakdancer.</h3>
+				<p class="lead">He is part of the French crew Pockemon Crew and the all-star team LEGION X. He has had a black belt in Kung Fu since he was sixteen. He practices Islam and can speak Algerian-Arabic, French and English. He is one of only three competitors to have won the Red Bull BC One twice.</p>
 			</div>
 		</div>
 	</div>
