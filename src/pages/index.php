75e79c24deb7d9a3bfe1126175f68204f1e6eaaf
Nico Watine <nico@studioncreations.com>
2022-04-03 11:03:44 -0500

    Add signature png

--- src/pages/index.php
+++ src/pages/index.php
@@ -16,7 +16,7 @@ body-class: index
 	</div>
 </section>
 
-<section class="bio-section" style="padding-top: 140px; padding-bottom: 140px;">
+<section class="bio-section" style="padding-top: 140px; padding-bottom: 100px;">
 	<div class="grid-container">
 		<div class="grid-x grid-margin-x">
 			<div class="cell large-5">
@@ -24,8 +24,9 @@ body-class: index
 			</div>
 			<div class="cell large-7">
 				<h2 class="ff-whoa mb-40" style="font-size: 70px;color: #323232;">about lilou</h2>
-				<h3 class="font-weight-bold text-uppercase mb-40" style="font-size: 26px;">Ali Ramdani, better known by his stage name Lilou, is a French b-boy breakdancer.</h3>
-				<p class="lead">He is part of the French crew Pockemon Crew and the all-star team LEGION X. He has had a black belt in Kung Fu since he was sixteen. He practices Islam and can speak Algerian-Arabic, French and English. He is one of only three competitors to have won the Red Bull BC One twice.</p>
+				<h3 class="font-weight-bold text-uppercase mb-20" style="font-size: 26px;">Ali Ramdani, better known by his stage name Lilou, is a French b-boy breakdancer.</h3>
+				<p class="lead mb-40">He is part of the French crew Pockemon Crew and the all-star team LEGION X. He has had a black belt in Kung Fu since he was sixteen. He practices Islam and can speak Algerian-Arabic, French and English. He is one of only three competitors to have won the Red Bull BC One twice.</p>
+				<img src="/img/signature.png" alt="">
 			</div>
 		</div>
 	</div>
